6b7b3db71f98cc15850df242d3e91e49d7b3bd5b
shashank2839 <91687603+shashank2839@users.noreply.github.com>
2022-01-08 18:01:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let result = document.getElementById('result');
+function rslt(){
+        let num = document.getElementById('num').value
+        let x = document.getElementsByTagName("body")[0];
+        x.style.backgroundColor = "#e6e6e6";
+        for(let i = 1; i<=num; i++){
+            if(i % 3 ==0 && i % 5 == 0){
+                result.innerHTML +=("<br/> FizzBuzz");
+            }else if(i % 3 == 0){
+                result.innerHTML +=("<br/> Fizz");
+            }else if(i % 5 == 0){
+                result.innerHTML +=("<br/> Buzz");
+            }else{
+                result.innerHTML +=("<br/>" + i)
+            }
+        }
+    }
